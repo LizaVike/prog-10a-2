4ce9f9aca13ebc68a45cd9e7fa6bbd32d932d921
Liza Vike <114918801+LizaVike@users.noreply.github.com>
2022-10-17 11:24:48 +0000

    1 uzdevums

---
+++ main.py
@@ -0,0 +1,19 @@
+print("ievadi 3 leņķus")
+a=int(input("Ievadiet 1. leņķ: "))
+b=int(input("Ievadiet 2. leņķ: "))
+c=int(input("Ievadiet 3. leņķ: "))
+t=a+b+c
+if t>180:
+  print("tristuris neeksiste")
+if t<180:
+  print("tristuris neeksiste")
+elif a+b+c==180:
+  print("tristuris eksiste")
+if a==b==c: print("tas ir vienadmalu tristuris")
+elif a==90: print("tas ir taisnleņķa trijstūris")
+elif b==90: print("tas ir taisnleņķa trijstūris")
+elif c==90: print("tas ir taisnleņķa trijstūris")
+if a>90: print("tas ir platleņķa trijstūris")
+elif b>90: print("tas ir platleņķa trijstūris")
+elif c>90: print("tas ir platleņķa trijstūris")
+if a<90 and b<90 and c<90: print("tas ir šaurleņķa trijstūris")
